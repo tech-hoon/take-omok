0de3c923916691ef55412688268f28b1841cf45d
taekhun <nth9708@naver.com>
2021-01-27 19:35:00 +0900

    :lipstick: board styling

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -1,6 +1,6 @@
 import "./Board.css";
 import { useState, useEffect } from "react";
-const LINE_NUMBER = 3;
+const LINE_NUMBER = 15;
 
 const Board = () => {
   //1 : black,  2 : white
@@ -9,68 +9,56 @@ const Board = () => {
     Array.from(Array(LINE_NUMBER), () => Array(LINE_NUMBER).fill(0))
   );
 
-  const isValid = ([M, N]) => {
-    if (array[M][N] !== 0) {
-      alert("이미 존재하는 돌입니다.");
-      return false;
-    } else if (array[M][N]) {
-    }
-
-    return true;
-  };
+  // stone 개행
+  (function () {
+    // console.log(document.querySelectorAll(".stones:nth-child(15n)"));
+  })();
 
   const passValue = ({ target: { id } }) => {
     // id => M,N
     const [M, N] = id.split("-");
     if (isValid([M, N]) === true) {
       array[M][N] = turn;
+      putStone(id);
+
       turn === 1 ? setTurn(2) : setTurn(1);
       //Todo : socket으로 값 전달
-      putStone(id);
     }
+  };
+
+  const isValid = ([M, N]) => {
+    if (array[M][N] !== 0) {
+      alert("이미 존재하는 돌입니다.");
+      return false;
+    }
+    //Todo : 승리 판정
+    return true;
   };
 
   const putStone = (id) => {
     const stone = document.getElementById(id);
-    // stone.style.visibility = "visible";
+    stone.style.borderRadius = "50%";
     stone.style.backgroundColor = turn === 1 ? "black" : "white";
   };
 
   useEffect(() => {
     console.log(`${turn === 1 ? "흑" : "백"}차례입니다.`);
-    console.log(array);
   });
 
   return (
     <>
       <div className='board'>
-        <button onClick={passValue} className='stones' id='0-0'>
-          A
-        </button>
-        <button onClick={passValue} className='stones' id='0-1'>
-          B
-        </button>
-        <button onClick={passValue} className='stones' id='0-2'>
-          C
-        </button>
-        <button onClick={passValue} className='stones' id='1-0'>
-          D
-        </button>
-        <button onClick={passValue} className='stones' id='1-1'>
-          E
-        </button>
-        <button onClick={passValue} className='stones' id='1-2'>
-          F
-        </button>
-        <button onClick={passValue} className='stones' id='2-0'>
-          G
-        </button>
-        <button onClick={passValue} className='stones' id='2-1'>
-          H
-        </button>
-        <button onClick={passValue} className='stones' id='2-2'>
-          I
-        </button>
+        {array.map((_, i) => {
+          return array.map((_, j) => {
+            return (
+              <button
+                onClick={passValue}
+                className='stones'
+                id={`${i}-${j}`}
+              ></button>
+            );
+          });
+        })}
       </div>
     </>
   );
