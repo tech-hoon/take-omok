8324c1fb3c9aee504def7562fdf2d86cfaf84f88
taekhun <nth9708@naver.com>
2021-01-27 11:58:17 +0900

    :lipstick: styling

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 const LINE_NUMBER = 3;
 
 const Board = () => {
+  //1 : black,  2 : white
   const [turn, setTurn] = useState(1);
   let [array, setArray] = useState(
     Array.from(Array(LINE_NUMBER), () => Array(LINE_NUMBER).fill(0))
@@ -12,7 +13,9 @@ const Board = () => {
     if (array[M][N] !== 0) {
       alert("이미 존재하는 돌입니다.");
       return false;
+    } else if (array[M][N]) {
     }
+
     return true;
   };
 
@@ -22,50 +25,53 @@ const Board = () => {
     if (isValid([M, N]) === true) {
       array[M][N] = turn;
       turn === 1 ? setTurn(2) : setTurn(1);
+      //Todo : socket으로 값 전달
       putStone(id);
     }
   };
 
   const putStone = (id) => {
     const stone = document.getElementById(id);
+    // stone.style.visibility = "visible";
     stone.style.backgroundColor = turn === 1 ? "black" : "white";
   };
 
+  useEffect(() => {
+    console.log(`${turn === 1 ? "흑" : "백"}차례입니다.`);
+    console.log(array);
+  });
+
   return (
     <>
-      <div>
-        <button onClick={passValue} id='0-0'>
+      <div className='board'>
+        <button onClick={passValue} className='stones' id='0-0'>
           A
         </button>
-        <button onClick={passValue} id='0-1'>
+        <button onClick={passValue} className='stones' id='0-1'>
           B
         </button>
-        <button onClick={passValue} id='0-2'>
+        <button onClick={passValue} className='stones' id='0-2'>
           C
         </button>
-        <br />
-        <button onClick={passValue} id='1-0'>
+        <button onClick={passValue} className='stones' id='1-0'>
           D
         </button>
-        <button onClick={passValue} id='1-1'>
+        <button onClick={passValue} className='stones' id='1-1'>
           E
         </button>
-        <button onClick={passValue} id='1-2'>
+        <button onClick={passValue} className='stones' id='1-2'>
           F
         </button>
-        <br />
-        <button onClick={passValue} id='2-0'>
+        <button onClick={passValue} className='stones' id='2-0'>
           G
         </button>
-        <button onClick={passValue} id='2-1'>
+        <button onClick={passValue} className='stones' id='2-1'>
           H
         </button>
-        <button onClick={passValue} id='2-2'>
+        <button onClick={passValue} className='stones' id='2-2'>
           I
         </button>
       </div>
-      <br></br>
-      {turn}
     </>
   );
 };
