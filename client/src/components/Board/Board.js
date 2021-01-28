457ed88562d5b5d0fe5018355509aa019d779486
taekhun <nth9708@naver.com>
2021-01-28 21:29:34 +0900

    :white_check_mark: isFive done.

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -31,46 +31,130 @@ const Board = () => {
     if (isValid([M, N]) === true) {
       array[M][N] = turn;
       putStone(id);
+
+      if (isFive([M, N])) {
+        turn === 1
+          ? alert("흑돌이 승리하였습니다.")
+          : alert("백돌이 승리하였습니다.");
+      }
       turn === 1 ? setTurn(2) : setTurn(1);
       console.log(array);
       //Todo : socket으로 값 전달
     }
   };
 
-  //Todo : 좌->우 인식 X
-  //Todo : 상,하
-  //Todo : 대각선 좌우
-  const isWin = ([M, N]) => {
-    let i = Number(M);
-    let j = Number(N);
-    let count = 1;
+  const isFive = ([M, N]) => {
+    M = Number(M);
+    N = Number(N);
 
-    console.log("TURN:" + typeof turn);
-    console.log("TEST:" + array[M][N + 1]);
-    console.log("TEST:" + array[M][N]);
-    console.log("TEST:" + array[M][N - 1]);
+    let count = 1;
+    let i = M;
+    let j = N;
 
     //왼쪽에 붙일때
-    while (count < 5) {
-      if (j == 15) break;
+    while (count < 6) {
+      if (j == 14) break;
       else if (array[i][j + 1] == turn) {
         count += 1;
         j = j + 1;
       } else break;
     }
 
+    //오른쪽에 붙일때
     i = M;
     j = N;
-    //오른쪽에 붙일때
-    while (count < 5) {
-      if (j == -1) break;
+    while (count < 6) {
+      if (j == 0) break;
       else if (array[i][j - 1] == turn) {
         count += 1;
         j = j - 1;
       } else break;
     }
 
-    turn == 1 ? console.log(`흑 ${count}`) : console.log(`백 ${count}`);
+    if (count == 5) {
+      return true;
+    }
+
+    //아래쪽에 붙일때
+    count = 1;
+    i = M;
+    j = N;
+    while (count < 6) {
+      if (i == 0) break;
+      else if (array[i - 1][j] == turn) {
+        count += 1;
+        i = i - 1;
+      } else break;
+    }
+
+    //아래쪽에 붙일때
+    i = M;
+    j = N;
+    while (count < 6) {
+      if (i == 14) break;
+      else if (array[i + 1][j] == turn) {
+        count += 1;
+        i = i + 1;
+      } else break;
+    }
+
+    if (count == 5) {
+      return true;
+    }
+
+    // ↙ 위에 붙일때
+    count = 1;
+    i = M;
+    j = N;
+    while (count < 6) {
+      if (i == 14 || j == 0) break;
+      else if (array[i + 1][j - 1] == turn) {
+        count += 1;
+        i = i + 1;
+        j = j - 1;
+      } else break;
+    }
+
+    // ↗ 아래에 붙일때
+    i = M;
+    j = N;
+    while (count < 6) {
+      if (i == 0 || j == 14) break;
+      else if (array[i - 1][j + 1] == turn) {
+        count += 1;
+        i = i - 1;
+        j = j + 1;
+      } else break;
+    }
+
+    if (count == 5) {
+      return true;
+    }
+
+    // ↘ 위에 붙일 때
+    count = 1;
+    i = M;
+    j = N;
+    while (count < 6) {
+      if (i == 14 || j == 14) break;
+      else if (array[i + 1][j + 1] == turn) {
+        count += 1;
+        i = i + 1;
+        j = j + 1;
+      } else break;
+    }
+
+    // ↖ 아래에 붙일 때
+    i = M;
+    j = N;
+    while (count < 6) {
+      if (i == 0 || j == 0) break;
+      else if (array[i - 1][j - 1] == turn) {
+        count += 1;
+        i = i - 1;
+        j = j - 1;
+      } else break;
+    }
 
     return count == 5 ? true : false;
   };
@@ -78,11 +162,6 @@ const Board = () => {
   const isValid = ([M, N]) => {
     if (array[M][N] !== 0) {
       alert("이미 존재하는 돌입니다.");
-      return false;
-    } else if (isWin([M, N])) {
-      turn === 1
-        ? alert("흑돌이 승리하였습니다.")
-        : alert("백돌이 승리하였습니다.");
       return false;
     }
     return true;
@@ -95,8 +174,7 @@ const Board = () => {
   };
 
   useEffect(() => {
-    // console.log(`${turn === 1 ? "흑" : "백"}차례입니다.`);
-    // console.log(array);
+    console.log(`${turn === 1 ? "흑" : "백"}차례입니다.`);
   });
 
   return (
